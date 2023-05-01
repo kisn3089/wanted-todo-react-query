b496c20c1c3bb3a4493dd7d2a5b565db8c240895
Stemps <kisn3089@naver.com>
2023-05-01 22:57:45 +0900

    [Deploy]BASE_URL 수정

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,9 @@
-import { createConnection } from "./models/db";
-import app from "./app";
+import { createConnection } from './models/db';
+import app from './app';
 
 createConnection();
 
-const port = process.env.PORT || 8080;
+export const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Now listening on port ${port}`);
